c99002ecf95604096789636572fc840df26bcf0b
ramya0705 <ryramyayalla@gmail.com>
2026-05-04 15:59:03 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -3,15 +3,15 @@
 #source .bashrc
 
 #! /bin/bash
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops
 sudo mv kops /usr/local/bin/kops
 chmod +x kubectl
 mv kubectl /usr/local/bin/kubectl
-
-aws s3api create-bucket --bucket mustafa.project.flm.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket mustafa.project.flm.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://mustafa.project.flm.k8s.local
-kops create cluster --name mustafa.k8s.local --zones us-east-1a,us-east-1b --master-count=1 --master-size t2.large --master-volume-size=30 --node-count=3 --node-size t2.medium --node-volume-size=20
-kops update cluster --name mustafa.k8s.local --yes --admin
+export KOPS_STATE_STORE=s3://ramya.ch.k8s
+kops create cluster --name ramya.k8s.local --cloud=aws --zones eu-north-1c,eu-north-1b,eu-north-1a --control-plane-size t3.small --control-plane-count 1 --control-plane-volume-size 30 --node-size t3.small --node-count 2 --node-volume-size 20
+kops update cluster --name ramya.k8s.local --yes --admin
